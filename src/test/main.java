f96dd8c023a5a810fceddd480b252604afb70c2c
qdr123 <tmxjdydrb12@gmail.com>
2020-09-19 02:16:14 +0900

    test

---
+++ src/test/main.java
@@ -0,0 +1,21 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 1; 
+		int b = 2;
+		
+		for(int i = 0 ;i < 10 ; i++) {
+			a++;
+			
+		}
+		
+		System.out.print(a+b);
+		
+		
+	}
+
+}
